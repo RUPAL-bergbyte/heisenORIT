709d6f40ae7eb823fa5fce83ab8e254d850f3d05
RUPAL-bergbyte <rupalverma.8.c@gmail.com>
2026-06-15 21:23:18 +0530

    Improve PR patch filtering

--- ai-pr-reviewer/pr_review.py
+++ ai-pr-reviewer/pr_review.py
@@ -1,7 +1,8 @@
-from github import Github, Auth
-from dotenv import load_dotenv
+import ollama
 import os
 
+from github import Github, Auth
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -35,7 +36,6 @@ pr = repo.get_pull(
     pr_number
 )
 
-
 print(
     "\nChanged files:\n"
 )
@@ -43,18 +43,119 @@ print(
 
 for file in pr.get_files():
 
+    if not (
+        file.filename.endswith(".py")
+        or file.filename.endswith(".js")
+        or file.filename.endswith(".cpp")
+        or file.filename.endswith(".java")
+    ):
+        continue
+
+
     print(
         file.filename
     )
+
+    if not file.patch:
+        continue
+
+
+    clean_patch = file.patch
+
+
+ignore_words = [
+    "PROMPT",
+    "Review only",
+    "Focus on:"
+]
+
+
+lines = []
+
+
+for line in clean_patch.split("\n"):
+
+    skip = False
+
+    for word in ignore_words:
+
+        if word in line:
+            skip = True
+
+    if not skip:
+        lines.append(line)
+
+
+clean_patch = "\n".join(lines)
+
+
+if len(clean_patch) > 2500:
+
+    clean_patch = clean_patch[:2500]
+
+
+    if len(clean_patch) > 3000:
+
+        clean_patch = (
+            clean_patch[:3000]
+        )
+
 
     print(
         "\nPATCH:\n"
     )
 
     print(
-        file.patch
+        clean_patch
     )
+
 
     print(
         "\n=================\n"
+    )
+
+
+    response = ollama.chat(
+
+        model="qwen2.5-coder:3b",
+
+        messages=[
+
+            {
+                "role": "system",
+
+                "content": """
+Review ONLY changed code.
+
+Ignore:
+- prompts
+- generated files
+- README
+- comments
+
+Focus on:
+- bugs
+- security
+- quality
+- performance
+"""
+            },
+
+            {
+                "role": "user",
+
+                "content": clean_patch
+            }
+
+        ]
+    )
+
+
+    print(
+        "\n========== REVIEW ==========\n"
+    )
+
+
+    print(
+        response["message"]["content"]
     )
